facb1301ae5ad8b31c377faf34a9cfd948bb5d43
SajalRawat <71058080+SajalRawat@users.noreply.github.com>
2025-12-06 14:41:55 +0530

    Applied Updated Designs
    credit of Designing : @rohitpimpale07-dot and @Abhinav01-create

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import Loader from '../../components/Loader/Loader'
 import './Home.css'
 import Logocollections from "../../assets/clublogos.svg"
 import Synclogo from "../../assets/synclogo.svg"
+import BgHome from "../../assets/homebg.svg"
+import SyncAIT from "../../assets/syncaithome.svg"
 
 export default function Home() {
 	/*
@@ -20,37 +22,24 @@ export default function Home() {
 	}
 	*/
 	return (
-		<section id="home" className="home-hero">
-			<div className="syncLogoDiv">
-				<img src={Synclogo} alt="" />
-			</div>
+		<section id="home" className="home-hero" >
+			
 			<div className="page-head flex  font-[Poppins]">
-				<div className='hero-div' >
-					<h1 className="blue-hero hero">A Place</h1>
-					<h1 className="blue-hero hero"><span className='text-red-500'>Where Every</span></h1>
-					<h1 className="blue-hero hero">Club Finds a</h1>
+				<div className='hero-div'  >
+					<img src={Synclogo} alt="" />
+					<img src={SyncAIT} alt=""  className='syncAITdesigned'  style={{margin:"40px 0 0 0 "}}/>
+					<h1 className="blue-hero hero">A Place Where</h1>
+					<h1 className="blue-hero hero">Every Club Finds a</h1>
 					<h1 className="gray-hero hero">Home</h1>
 					<div className='secondary-hero-div'>
-						<p className='line1'>
-							A one-stop solution to all the announcement and 
-						</p>
-						<p className='line1'>
-							notification clutter from clubs across campus.
-						</p>
-						<p className='line2'>
-							Now Made Easier than Ever Before!
-						</p>
-						<p>
-
-						</p>
-						<div className='flex hero-btn-div'>
-							<button className='hero-btn bg-blue-300 font-medium'>ANNOUNCEMENTS</button>
-							<button className='hero-btn font-medium'>ALL CLUBS</button>
-						</div>
 					</div>
 				</div>
 				<div className='clubCollectionDiv'>
 					<img className='w-[70vw] clubCollection' src={Logocollections} alt="" />
+					<div className='flex hero-btn-div'>
+							<button className='hero-btn bg-blue-300 font-medium'>ANNOUNCEMENTS</button>
+							<button className='hero-btn font-medium'>ALL CLUBS</button>
+						</div>
 				</div>
 			</div>
 		</section>
